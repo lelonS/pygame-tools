899516a5b28fb0ce383714318aa4505983fcd8df
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-13 15:52:48 +0100

    Update ui_element.py

--- ui_classes/ui_element.py
+++ ui_classes/ui_element.py
@@ -2,4 +2,22 @@ import pygame
 
 
 class UIElement:
-    pass
+    rect: pygame.Rect
+    background_color: tuple[int, int, int]
+    draw_background: bool
+
+    def __init__(self,
+                 rect: pygame.Rect,
+                 background_color: tuple[int, int, int] = (0, 0, 0),
+                 draw_background: bool = True):
+
+        self.rect = rect
+        self.background_color = background_color
+        self.draw_background = draw_background
+
+    def draw(self, surface: pygame.Surface):
+        if self.draw_background:
+            pygame.draw.rect(surface, self.background_color, self.rect)
+
+    def update(self, event: pygame.event.Event):
+        pass
