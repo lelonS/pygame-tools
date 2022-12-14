001cf3abb348a4e49730233b34c3d91de71ba729
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-14 20:45:46 +0100

    Update button.py

--- ui_classes/button.py
+++ ui_classes/button.py
@@ -1,11 +1,23 @@
 import pygame
-from ui_classes.ui_element import UIElement
+from ui_classes.ui_element import UIElement, TOP_LEFT, CENTER, get_rect
 from ui_classes.label import TextLabel
 
 
 class Button(UIElement):
-    label: TextLabel
     _hovered: bool
     on_click: callable
     on_hover: callable
     on_unhover: callable
+
+    def __init__(self, rect: pygame.Rect, **kwargs):
+        anchor = kwargs.get("anchor", TOP_LEFT)
+        background_color = kwargs.get("background_color", (0, 0, 0))
+        draw_background = kwargs.get("draw_background", True)
+
+        r = get_rect(rect.x, rect.y, rect.width, rect.height, anchor)
+        super().__init__(r, background_color, draw_background)
+
+        self._hovered = False
+        self.on_click = None
+        self.on_hover = None
+        self.on_unhover = None
