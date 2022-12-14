d4c8dab4d76088d6ed81b1c7c906e23c846cf0dd
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-14 16:55:20 +0100

    Update button.py

--- ui_classes/button.py
+++ ui_classes/button.py
@@ -1,6 +1,11 @@
 import pygame
 from ui_classes.ui_element import UIElement
+from ui_classes.label import TextLabel
 
 
 class Button(UIElement):
-    pass
+    label: TextLabel
+    _hovered: bool
+    on_click: callable
+    on_hover: callable
+    on_unhover: callable
